dcf899027be1691e970ac2cf6f59d0c546debfe0
t4t5u0 <ymmtryk0902@gmail.com>
2021-08-31 19:54:21 +0900

    Update: 正規表現をきれいにした

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -1,14 +1,12 @@
 chrome.browserAction.onClicked.addListener((tab: chrome.tabs.Tab) => {
   // alert("before")
   console.log("before");
-  url2md(tab);
-  // alert("after")
-  console.log("after");
+  url2md(tab).then(()=> {console.log("after")});
 });
 
-const url2md = (tab: chrome.tabs.Tab) => {
+const url2md = async (tab: chrome.tabs.Tab) => {
   const url = tab.url;
-  const pattern = /(.jpe?g)|(.png)|(.gif)|(.webp)$/;
+  const pattern = /\.(jpe?g|png|gif|webp)$/;
   const isImg = pattern.test(url);
   const title = tab.title;
   // []() | ![]()
@@ -16,10 +14,10 @@ const url2md = (tab: chrome.tabs.Tab) => {
 
   console.log(mdUrl);
 
-  setTimeout(() => {
-    navigator.clipboard.writeText(mdUrl);
-  }, 500);
-  navigator.clipboard.writeText(mdUrl);
+  // setTimeout(async () => {
+  //   await navigator.clipboard.writeText(mdUrl);
+  // }, 500);
+  // await navigator.clipboard.writeText(mdUrl);
   console.log("copied!");
   alert("url2md");
 };
